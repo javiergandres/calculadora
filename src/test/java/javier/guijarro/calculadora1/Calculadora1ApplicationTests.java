d03c20f95ba60e3d88ed9e4e7007c455cdf9e435
root <root@localhost.localdomain>
2019-04-11 18:40:22 +0200

    Commit con web

---
+++ src/test/java/javier/guijarro/calculadora1/Calculadora1ApplicationTests.java
@@ -0,0 +1,16 @@
+package javier.guijarro.calculadora1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Calculadora1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
